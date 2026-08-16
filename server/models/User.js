4f1044613d93bb498f487f988abf4807b85d5b32
samir-sah <sahsamir2004@gmail.com>
2026-08-16 13:25:27 +0530

    feat: add User and Contact models with schema definitions and password hashing

---
+++ server/models/User.js
@@ -0,0 +1,51 @@
+import mongoose from "mongoose";
+import bcrypt from "bcryptjs";
+
+const userSchema = new mongoose.Schema(
+    {
+    name: {
+        type: String,
+        required: [true, "Name is required"],
+        trim: true,
+    },
+
+    email: {
+        type: String,
+        required: [true, "Email is required"],
+        unique: true,
+        lowercase: true,
+        trim: true,
+        match: [/^\S+@\S+\.\S+$/, "Please provide a valid email"],
+
+    },
+
+    password: {
+        type: String,
+        required: [true, "Password is required"],
+        minlength: [6, "Password must be atleast 6 characters"],
+        select: false, //iska logic padhna ki kyu ye mention kiya hai
+    },
+
+    role: {
+        type: String,
+        enum: ["Owner","Member"],
+        default: "Owner",
+    },
+    company: {type: String, trim:true, default: ""},
+    avatar: { type: String, default:""},
+    },
+    { timestamps: true}   
+);
+
+userSchema.pre("save", async function(next) {
+    if(!this.isModified("password")) return next();
+    const salt = await bcrypt.genSalt(10);
+    this.password = await bcrypt.hash(this.password, salt);
+    next();
+});
+
+userSchema.methods.matchPassword = function (entered){
+    return bcrypt.compare(entered, this.password);
+};
+
+export const User = mongoose.model("User", userSchema);
